a17256d0b02f240a4bbae2953032d6f7dc3abfb2
Etienne Coppel <eco@ik.me>
2025-08-30 00:23:45 +0200

    Add Allowed_hosts var in settings.py

--- config/settings.py
+++ config/settings.py
@@ -18,6 +18,10 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
 
 
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS", cast=lambda v: [s.strip() for s in v.split(",")]
+)
+
 # Application definition
 
 INSTALLED_APPS = [
