cba88bb91c40336b82d333edf9688e36b5a52047
Etienne Coppel <eco@ik.me>
2025-09-02 20:48:27 +0200

    css and views corrections

--- main/views.py
+++ main/views.py
@@ -11,15 +11,6 @@ def home(request):
     context = {
         "bikers": bikers,
     }
-    #   bikers_path = os.path.join(settings.BASE_DIR, "static", "img", "bikers")
-    #   bikers_images = []
-
-    #    if os.path.exists(bikers_path):
-    #        for filename in os.listdir(bikers_path):
-    #            if filename.endswith(".jpg"):
-    #                bikers_images.append(f"img/bikers/{filename}")
-
-    #    context = {"bikers_images": bikers_images}
     return render(request, "main/home.html", context)
 
 
